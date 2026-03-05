74c3fe54fab26d5dd5eb165ba055b84e9e8416b7
David Goossens <4958548+dielaenge@users.noreply.github.com>
2026-03-05 16:02:05 +0100

    refactor: symbol naming and print statements updated

--- phase-4-serverless_deployment/src/model_processor.py
+++ phase-4-serverless_deployment/src/model_processor.py
@@ -16,13 +16,16 @@ class AcousticModelProcessor:
         self.output_names = [output.name for output in self.sess.get_outputs()] # With the BAPE model, there are 2 output features to store => store the name for each feature in an array
         
         logger.info("Model initialized successfully.")
-        logger.info("Input Name: %s, Output Names: %s, Additional Outputs: preprocessed wav-input and spectrogram", self.input_name, self.output_names)
+        logger.info("Input Name: %s, Output Names: %s", self.input_name, self.output_names)
 
-    def generate_vector(self, preprocessed_spectogram: np.ndarray) -> dict: #after model and inference session are initialized, a function to define the vector is set up which takes in the preprocessed audio from the model
+    def run_inference(self, preprocessed_spectogram: np.ndarray) -> dict: #after model and inference session are initialized, define input_feed, a dictionary with spectrogram inputs
         """Runs the ONNX inference session and returns a dictionary of all model outputs."""
-
-        input_feed = {self.input_name: preprocessed_spectogram} #creates a dictionary called input_feed required to run the InferenceSession
-        all_outputs = self.sess.run(self.output_names, input_feed) #the BAPE model generates a set of outputs from a single input: one `latent` vector of shape `(1, 1024)` and `latent_weights` vector of shape `(1, 256)`
+        input_feed = {self.input_name: preprocessed_spectogram} 
+        # BAPE model generates multiple outputs: 
+            # N `latent` vectors of shape `(N, 1024)` – The acoustic fingerprint
+            # quantiles which describe uncertainty in the fingerprint
+            # estimated_params in a 7 x 3 Matrix (7 octave bands, for each an estimate + an upper and lower end for each estimate describing the confidence interval) 
+        all_outputs = self.sess.run(self.output_names, input_feed) 
         results = {
             name: array for name, 
             array in zip(self.output_names, all_outputs)
