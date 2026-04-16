7717ad41ef569919ec594a1168bfe71d35ec6373
David Goossens <4958548+dielaenge@users.noreply.github.com>
2026-04-16 19:25:58 +0200

    Added output for CloudFront URL for phase 5.

--- phase-5-container-orchestration/terraform/outputs.tf
+++ phase-5-container-orchestration/terraform/outputs.tf
@@ -3,3 +3,7 @@ output "bape-inference-tf-repository_url" {
   description = "Repository URL for bape-inference-tf ECR repository"
   value       = aws_ecr_repository.bape-inference-tf.repository_url
 }
+
+output "cloudfront_phase5_url" {
+  value =aws_cloudfront_distribution.bape_phase5_frontend_s3_distribution.domain_name
+}
