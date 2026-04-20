8113ba8e493a61a4fcbdf1a4559bdc25b2f6c4ac
David Goossens <4958548+dielaenge@users.noreply.github.com>
2026-04-20 11:03:20 +0200

    Added  ALB origin and ordered cache behaviour for the new origin, based on managed caching policy.

--- phase-5-container-orchestration/terraform/cdn.tf
+++ phase-5-container-orchestration/terraform/cdn.tf
@@ -1,14 +1,12 @@
 resource "aws_s3_bucket" "bape_phase5_frontend" {
-    bucket = "bape-phase5-frontend-davidg"
+  bucket = "bape-phase5-frontend-davidg"
 
   tags = {
     Name = "bape_phase5_frontend_bucket"
   }
 }
 
-locals {
-    s3_origin_id = "bape-phase5-frontend-s3-distribution"
-}
+
 
 # See https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/private-content-restricting-access-to-s3.html
 data "aws_iam_policy_document" "origin_bucket_policy" {
@@ -22,8 +20,8 @@ data "aws_iam_policy_document" "origin_bucket_policy" {
     }
 
     actions = [
-      "s3:GetObject"    
-      ]
+      "s3:GetObject"
+    ]
 
     resources = [
       "${aws_s3_bucket.bape_phase5_frontend.arn}/*",
@@ -49,19 +47,52 @@ resource "aws_cloudfront_origin_access_control" "bape_phase5_oac" {
   signing_protocol                  = "sigv4"
 }
 
+locals {
+  s3_origin_id = "bape-phase5-frontend-s3-distribution"
+}
+
 resource "aws_cloudfront_distribution" "bape_phase5_frontend_s3_distribution" {
+  enabled = true
+
   origin {
     domain_name              = aws_s3_bucket.bape_phase5_frontend.bucket_regional_domain_name
-    origin_id = local.s3_origin_id
+    origin_id                = local.s3_origin_id
+    origin_access_control_id = aws_cloudfront_origin_access_control.bape_phase5_oac.id
   }
 
-  enabled             = true
-  
+  origin {
+    domain_name = aws_lb.bape_alb.dns_name
+    origin_id   = aws_lb.bape_alb.id
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+
   default_cache_behavior {
     allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
     cached_methods   = ["GET", "HEAD"]
     target_origin_id = local.s3_origin_id
 
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  ordered_cache_behavior {
+    path_pattern    = "/acou-vec/*"
+    allowed_methods = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods  = ["GET", "HEAD"]
+    #CachingDisabled Policy
+    cache_policy_id        = "4135ea2d-6df8-44a3-9df3-4b5a84be39ad"
+    target_origin_id       = aws_lb.bape_alb.id
     viewer_protocol_policy = "redirect-to-https"
   }
 
