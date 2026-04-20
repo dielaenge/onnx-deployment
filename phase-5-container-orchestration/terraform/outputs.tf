1d25a6e1a938d4c1e2691af3d103e469630aeca5
David Goossens <4958548+dielaenge@users.noreply.github.com>
2026-04-20 10:56:28 +0200

    Added descriptions to outputs. Added ALB DNS name.

--- phase-5-container-orchestration/terraform/outputs.tf
+++ phase-5-container-orchestration/terraform/outputs.tf
@@ -5,5 +5,11 @@ output "bape-inference-tf-repository_url" {
 }
 
 output "cloudfront_phase5_url" {
-  value =aws_cloudfront_distribution.bape_phase5_frontend_s3_distribution.domain_name
+  description = "Edge distribution URL serving the BAPE frontend."
+  value       = aws_cloudfront_distribution.bape_phase5_frontend_s3_distribution.domain_name
+}
+
+output "bape_alb_dns_name" {
+  description = "DNS name of phase 5 ALB."
+  value       = aws_lb.bape_alb.dns_name
 }
