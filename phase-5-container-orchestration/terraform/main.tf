ef02515a3c665caec576f960b743789bec5a9174
David Goossens <4958548+dielaenge@users.noreply.github.com>
2026-04-23 10:01:56 +0200

    updated ECS container definition to :latest for CI/CD implementation

--- phase-5-container-orchestration/terraform/main.tf
+++ phase-5-container-orchestration/terraform/main.tf
@@ -440,7 +440,7 @@ resource "aws_ecs_task_definition" "task_definition_bape" {
   container_definitions = jsonencode([
     {
       name      = "bape-container"
-      image     = "${aws_ecr_repository.bape-inference-tf.repository_url}:v3.0.0-cloudfront-edge"
+      image     = "${aws_ecr_repository.bape-inference-tf.repository_url}:latest"
       essential = true
       portMappings = [
         {
