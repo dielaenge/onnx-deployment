c92ddc9c35399e0e10735984318c1baa3b123c06
David Goossens <4958548+dielaenge@users.noreply.github.com>
2026-05-15 17:36:39 +0200

    REFACTOR: Deleted redundant AcousticModelProcessor, now imported from audio_utils.

--- phase-6-production-ready/app/main.py
+++ phase-6-production-ready/app/main.py
@@ -5,71 +5,22 @@ import logging
 from pathlib import Path
 import numpy as np
 from contextlib import asynccontextmanager
-from librosa.feature import melspectrogram
-import torch
-from torch import Tensor
 
 # Identify Base Directory
 BASE_DIR = Path(__file__).resolve().parent
 
 from .inference_engine import AcousticModelProcessor
-from .audio_utils import preprocess_audio
+from .audio_utils import MelSpectrogram
 
 # --- Logging Setup ---
 logging.basicConfig(
     level=logging.INFO,
     format= '%(asctime)s - %(name)s %(levelname)s - %(message)s'
 )
-logger = logging.getLogger("API")
+logger = logging.getLogger(__name__)
 
 # --- ONNX model path ---
 MODEL_PATH = BASE_DIR / "models" / "bape_2026-04-13_15-13-22.onnx"
-
-# --- Preprocessing class `MelSpectrogram` copied from [BAPE repository: bape/src/util/signals.py](https://github.com/philipp-goetz/bape/blob/7988f939d1c69301e31d322fecbbaa2a031ef3e1/src/util/signals.py) and adapted (see comments) for deployment---
-
-class MelSpectrogram:
-    """Spectrogram with a mel frequency scale"""
-    def __init__(
-        self, 
-        sr: float = 16000.0, 
-        n_fft: int = 64, 
-        hop_size: int = 16,
-        n_mels: int = 17, 
-        fmin: float = 100.0, 
-        fmax: float = 8000,
-        power: float = 2.0, 
-        log_mag: bool = False, 
-        # c_mag: Optional[float] = None, (not used for SpeechEncoder model)
-        trunc: int | None = None,
-) -> None:
-        self.sr, self.n_fft, self.hop_size, self.n_mels = sr, n_fft, hop_size, n_mels
-        self.fmin, self.fmax, self.power, self.log_mag = fmin, fmax, power, log_mag
-        self.trunc = trunc
-        # self.freqs = mel_frequencies(n_mels=n_mels, fmin=fmin, fmax=fmax) #not used in the __call__ function
-    
-    def __call__(self, input_signal: np.ndarray) -> np.ndarray:
-        """Takes 1D audio signal as input and returns the melspectrogram as tensor."""
-
-        # From here until `return` statement code is copied from BAPE repo
-        spec = melspectrogram(
-            y=input_signal, sr=self.sr, n_fft=self.n_fft, hop_length=self.hop_size,
-            n_mels=self.n_mels, fmin=self.fmin, fmax=self.fmax, power=1.0,
-        )
-        spec = Tensor(spec)
-        spec /= spec.max()
-        spec = spec.pow(self.power)
-        if self.log_mag:
-            spec = 10 * torch.log10(spec + 1e-12)
-        if self.trunc is not None:
-            nbins, length = spec.size()
-            if length < self.trunc:
-                spec = torch.cat(
-                    (spec, torch.zeros((nbins, self.trunc - length))), dim=-1
-                )
-            else:
-                spec = spec[:, : self.trunc]
-
-        return spec
 
 # contextlib.asynccontextmanager used witht the lifespan parameter is recommended for defining app startup and shutdown in FastAPI
 
@@ -181,22 +132,17 @@ async def websocket_endpoint(websocket: WebSocket):
                 }
 
                 # Print the shape to confirm, and just the first 3 parameter estimates 
-                logger.info("Inference complete.\nFirst three T60 Params: %s", response_payload['params'][0][0][:3])
+                logger.info("Inference results for last 4 seconds received.")
+                logger.info("First three T60 Params: %s", response_payload['params'][0][0][:3])
 
                 # send results
                 await websocket.send_json(response_payload)
-                
-                # confirm completed inference
-                await websocket.send_text("DUMMY: Inference complete for window.")
 
                 # slice buffer to keep the latter half of the window (2 seconds; which will be concatenated with new input if available)
                 audio_buffer = audio_buffer[-32000:]
 
     except Exception as e:
         logger.exception("Error in Websocket: %s", e)
-        await websocket.send_text("ERROR: Internal server error.")
-        await websocket.close()
-
 
 # LAUNCH
 if __name__ == "__main__":
