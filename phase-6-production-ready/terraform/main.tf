51e95802a82706f43be73a9b4dafdc4d6c496258
David Goossens <4958548+dielaenge@users.noreply.github.com>
2026-05-18 12:56:20 +0200

    FIX: CICD deploy-phase6 workflow now pointing to correct bape-app-data-phase6-davidg/models/ bucket

--- phase-6-production-ready/terraform/main.tf
+++ phase-6-production-ready/terraform/main.tf
@@ -8,7 +8,7 @@ data "aws_availability_zones" "available" {
   state = "available"
 }
 # -------------------------------
-# S3 BUCKET FOR PHASE 5 APP DATA
+# S3 BUCKET FOR PHASE 6 APP DATA
 # -------------------------------
 resource "aws_s3_bucket" "bape_app_data_phase6" {
   bucket = "bape-app-data-phase6-davidg"
