2f4d34bbdedfdd46dbcaa1a780499bfb1400d337
David Goossens <4958548+dielaenge@users.noreply.github.com>
2026-08-01 20:49:20 +0200

    chore: removed unused BASE_DIR variable

--- phase-6-production-ready/app/inference_engine.py
+++ phase-6-production-ready/app/inference_engine.py
@@ -3,9 +3,6 @@ import numpy as np
 from pathlib import Path
 import logging
 
-# Identify Base Directory
-BASE_DIR = Path(__file__).resolve().parent
-
 logger = logging.getLogger(__name__)
 providers=["CPUExecutionProvider"]
 
