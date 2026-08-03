c1e0fe9d60ad15e9dfb241609cfa9a77595311cc
David Goossens <4958548+dielaenge@users.noreply.github.com>
2026-08-03 14:04:03 +0200

    fix: corrected task role for worker task definition; fixed syntax

--- phase-7/terraform/compute/ecs.tf
+++ phase-7/terraform/compute/ecs.tf
@@ -2,7 +2,7 @@
 # ECS
 # ---
 
-# TASK DEFINITION WEB CONTAINER
+# TASK DEFINITION MAIN CONTAINER
 resource "aws_ecs_task_definition" "task_definition_bape" {
   family                   = "task_definition_bape"
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
@@ -65,44 +65,44 @@ resource "aws_ecs_task_definition" "task_definition_bape" {
 resource "aws_ecs_task_definition" "task_definition_worker" {
   family                   = "task_definition_worker"
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
-  task_role_arn            = aws_iam_role.ecs_task_role.arn
+  task_role_arn            = aws_iam_role.ecs_worker_task_role.arn
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu                      = 512
   memory                   = 1024
   container_definitions = jsonencode([
     {
-      "name" : "worker-container"
-      "command" : ["python", "-m", "app.worker"]
-      "image" : "${data.aws_ecr_repository.bape_ecr.repository_url}:phase7"
-      "essential" : true
-      "environment" : [
+      "name" = "worker-container"
+      "command" = ["python", "-m", "app.worker"]
+      "image" = "${data.aws_ecr_repository.bape_ecr.repository_url}:phase7"
+      "essential" = true
+      "environment" = [
         {
-          "name" : "NUMBA_CACHE_DIR",
-          "value" : "/tmp"
+          "name" = "NUMBA_CACHE_DIR",
+          "value" = "/tmp"
         },
         {
-          "name" : "JOBLIB_TEMP_FOLDER",
-          "value" : "/tmp"
+          "name" = "JOBLIB_TEMP_FOLDER",
+          "value" = "/tmp"
         },
         {
-          "name" : "SQS_QUEUE_URL",
-          "value" : data.aws_sqs_queue.bape_queue.url
+          "name" = "SQS_QUEUE_URL",
+          "value" = data.aws_sqs_queue.bape_queue.url
         },
         {
-          "name" : "APP_DATA_BUCKET_NAME",
-          "value" : data.aws_s3_bucket.bape_bucket.id
+          "name" = "APP_DATA_BUCKET_NAME",
+          "value" = data.aws_s3_bucket.bape_bucket.id
         },
         {
-          "name" : "AWS_REGION",
-          "value" : data.aws_region.current.name
+          "name" = "AWS_REGION",
+          "value" = data.aws_region.current.name
         }
       ]
       logConfiguration = {
         logDriver = "awslogs",
         options = {
           "awslogs-group"  = aws_cloudwatch_log_group.log_group_ecs_bape_inference_phase7.name
-          "awslogs-region" = "eu-central-1"
+          "awslogs-region" = data.aws_region.current.name
           "awslogs-stream-prefix" : "worker-ecs-phase7_"
         }
       }
